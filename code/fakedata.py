3ea010964c997fd707a9b098c741daf046d49d5b
megbedell <megbedell@gmail.com>
2017-08-07 15:21:26 +0200

    generalized to four-line case

--- code/fakedata.py
+++ code/fakedata.py
@@ -25,61 +25,71 @@ def doppler(rv):
 def oned_gaussian(xs, mm, sig):
     return np.exp(-0.5 * (xs - mm) ** 2 / sig ** 2) / np.sqrt(2. * np.pi * sig)
 
-def make_synth(rv, xs, mm, sig):
+def make_synth(rv, xs, ds, ms, sig):
     """
     `rv`: radial velocity in m/s (or same units as `c` above
     `xs`: `[M]` array of wavelength values
-    `mm`: location of the line center in rest wavelength
+    `ds`: depth-like (really EW) parameters for lines
+    `ms`: locations of the line centers in rest wavelength
     `sig`: sigma of pixel-convolved LSF
     """
     synths = np.ones_like(xs)
-    synths -= oned_gaussian(xs * doppler(rv), mm, sig) / 8.
+    for d, m in zip(ds, ms):
+        synths *= np.exp(-d *
+            oned_gaussian(xs * doppler(rv), m, sig))
     return synths
 
-def make_mask(rv, xs, mm, w1, w2):
+def make_mask(rv, xs, ws, ms, w1, w2):
     """
     `rv`: radial velocity in m/s (or same units as `c` above
     `xs`: [M] array of wavelength values
-    `mm`: location of the center of the binary mask tophat in rest wavelength
+    `ws`: weights to apply to the binary mask tophats
+    `ms`: locations of the centers of the binary mask tophats in rest wavelength
     `w1`: half-width of a wavelength pixel
     `w2`: half-width of the binary mask tophat in rest wavelength
 
     Notes: The doppler "shift" dilates the input wavelengths,
       and also the w1 half-width.
+    
+    Bugs: Super-inefficient code.
     """
     assert w1 > 0
     assert w2 > 0
     drv = doppler(rv)
     ddd = np.abs(w1 * drv - w2)
     dd = w1 * drv + w2
-    xmms = xs * drv - mm
     model = np.zeros_like(xs)
-    model[xmms > -dd] = (dd + xmms[xmms > -dd]) / (dd - ddd)
-    model[xmms > -ddd] = 1.
-    model[xmms > ddd] = (dd - xmms[xmms > ddd]) / (dd - ddd)
-    model[xmms > dd] = 0.
+    for ww, mm in zip(ws, ms):
+        xmms = xs * drv - mm
+        dmodel = np.zeros_like(xs)
+        dmodel[xmms > -dd] = (dd + xmms[xmms > -dd]) / (dd - ddd)
+        dmodel[xmms > -ddd] = 1.
+        dmodel[xmms > ddd] = (dd - xmms[xmms > ddd]) / (dd - ddd)
+        dmodel[xmms > dd] = 0.
+        model += ww * dmodel
     return 1. - model
 
-def dsynth_dv(rv, xs, mm, sig):
+def dsynth_dv(rv, xs, ds, ms, sig):
     dv = 10. # m/s
-    f2 = make_synth(rv + dv, xs, mm, sig)
-    f1 = make_synth(rv - dv, xs, mm, sig)
+    f2 = make_synth(rv + dv, xs, ds, ms, sig)
+    f1 = make_synth(rv - dv, xs, ds, ms, sig)
     return (f2 - f1) / (2. * dv)
 
-def make_data(N, xs, mm, sig):
+def make_data(N, xs, ds, ms, sig):
     """
     `N`: number of spectra to make
     `xs`: `[M]` array of wavelength values
-    `mm`: location of the line center in rest wavelength
+    `ds`: depth-like parameters for lines
+    `ms`: locations of the line centers in rest wavelength
     `sig`: sigma of pixel-convolved LSF
     """
     M = len(xs)
     data = np.zeros((N, M))
     ivars = np.zeros((N, M))
-    rvs = 30000. * np.random.uniform(-1., 1., size=N)
+    rvs = 30000. * np.random.uniform(-1., 1., size=N) # 30 km/s bc Earth ; MAGIC
     for n, rv in enumerate(rvs):
-        ivars[n, :] = 10000.
-        data[n, :] = make_synth(rv, xs, mm, sig)
+        ivars[n, :] = 10000. # s/n = 100 ; MAGIC
+        data[n, :] = make_synth(rv, xs, ds, ms, sig)
         data[n, :] += np.random.normal(size=M) / np.sqrt(ivars[n, :])
     return data, ivars, rvs
 
@@ -143,37 +153,52 @@ def quadratic_max(xs, ys):
 
 if __name__ == "__main__":
     import pylab as plt
-                         
-    # set parameters
-    np.random.seed(42)
-    mm = 5000.0 # A
-    sig = 0.05 # A
-    dx = 0.01 # A
-    xs = np.arange(4998. + 0.5 * dx, 5002., dx) # A
+    singleline = False
+    
+    if singleline:
+        # set parameters
+        np.random.seed(42)
+        ds = [1. / 8., ] # EW units (A), sort-of
+        ws = [1., ] # dimensionless weights
+        ms = [5000.0, ] # A
+        sig = 0.05 # A
+        dx = 0.01 # A
+        xs = np.arange(4998. + 0.5 * dx, 5002., dx) # A
+        plotprefix = "single"
+    else:
+        # set parameters
+        np.random.seed(42)
+        ds = [1. / 2., 1. / 8., 1., 1. / 32.] # EW units (A), sort-of
+        ws = [1., 1., 1., 1.] # dimensionless weights
+        ms = np.arange(4999.25, 5001., 0.5)
+        sig = 0.05 # A
+        dx = 0.01 # A
+        xs = np.arange(4998. + 0.5 * dx, 5002., dx) # A
+        plotprefix = "four"
 
     # make and plot fake data
     N = 512
-    data, ivars, true_rvs = make_data(N, xs, mm, sig)
+    data, ivars, true_rvs = make_data(N, xs, ds, ms, sig)
     plt.clf()
     for n in range(8):
         plt.step(xs, data[n, :] + 0.25 * n, color="k")
     plt.title("examples of (fake) data")
-    plt.savefig("data.png")
+    plt.savefig(plotprefix+"_data.png")
 
     # compute CRLBs
-    dmodel_dv = dsynth_dv(0., xs, mm, sig)
+    dmodel_dv = dsynth_dv(0., xs, ds, ms, sig)
     crlbs = np.zeros(N)
-    for n in range(N):
+    for n in range(N): # average CRLB; averaging over true RV
         crlbs[n] = np.sum(dmodel_dv * ivars[n, :] * dmodel_dv)
     crlb = 1. / np.sqrt(np.mean(crlbs))
     print("CRLB:", crlb, "m/s")
 
     # get best-fit velocities
     halfwidth = 0.075 # A; half-width of binary mask
-    options = ([make_synth, (xs, mm, sig), xcorr],
-               [make_synth, (xs, mm, sig), chisq],
-               [make_mask, (xs, mm, 0.5 * dx, halfwidth), xcorr],
-               [make_mask, (xs, mm, 0.5 * dx, halfwidth), chisq])
+    options = ([make_synth, (xs, ds, ms, sig), xcorr],
+               [make_synth, (xs, ds, ms, sig), chisq],
+               [make_mask, (xs, ws, ms, 0.5 * dx, halfwidth), xcorr],
+               [make_mask, (xs, ws, ms, 0.5 * dx, halfwidth), chisq])
     best_rvs = np.zeros((N, len(options)))
     for n in range(N):
         if n == 0:
@@ -186,7 +211,7 @@ if __name__ == "__main__":
                 plt.axvline(best_rvs[n, j], alpha=0.5)
         if n == 0:
             plt.title("grids of objective values")
-            plt.savefig("objective.png")
+            plt.savefig(plotprefix+"_objective.png")
 
     # plot best_rvs
     for j, options in enumerate(options):
@@ -198,7 +223,7 @@ if __name__ == "__main__":
         plt.title(titlestr)
         plt.xlabel("true RVs")
         plt.ylabel("measured RVs")
-        plt.savefig("rv_measurements_{:02d}.png".format(j))
+        plt.savefig(plotprefix+"_rv_measurements_{:02d}.png".format(j))
 
         plt.clf()
         plt.plot(true_rvs, best_rvs[:, j] - true_rvs, "k.", alpha=0.5)
@@ -207,29 +232,28 @@ if __name__ == "__main__":
         plt.title(titlestr)
         plt.xlabel("true RVs")
         plt.ylabel("RV mistake")
-        plt.ylim(-4500., 4500.)
-        plt.savefig("rv_mistakes_{:02d}.png".format(j))
+        plt.ylim(-500., 500.)
+        plt.savefig(plotprefix+"_rv_mistakes_{:02d}.png".format(j))
 
     # make and plot mask
     rvs -= np.min(rvs)
     plt.clf()
     for rv in rvs[::4]:
-        mask = make_mask(rv, xs, mm, 0.075, 0.5 * dx)
+        mask = make_mask(rv, xs, ws, ms, 0.075, 0.5 * dx)
         plt.step(xs, mask + 0.0011 * rv, "k-")
     plt.title("pixel-convolved binary masks")
-    plt.savefig("mask.png")
+    plt.savefig(plotprefix+"_mask.png")
 
     # make and plot synth model
     plt.clf()
     for rv in rvs[::4]:
-        model = make_synth(rv, xs, mm, sig)
+        model = make_synth(rv, xs, ds, ms, sig)
         plt.plot(xs, model + 0.0011 * rv, "k-")
     plt.plot(xs, 4096. * dmodel_dv, "r-")
     plt.title("models and velocity derivative (times 4096)")
-    plt.savefig("synth.png")
+    plt.savefig(plotprefix+"_synth.png")
 
-    # get best-fit velocities
-    halfwidth = 0.075 # A; half-width of binary mask
+    # look at rms as a function of binary-mask width
     method = xcorr
     template = make_mask
     tiny = 1. / 128.
@@ -238,7 +262,7 @@ if __name__ == "__main__":
     for j,halfwidth in enumerate(halfwidths):
         print(j)
         for n in range(N):
-            args = (xs, mm, 0.5 * dx, halfwidth)
+            args = (xs, ws, ms, 0.5 * dx, halfwidth)
             rvs, objs = get_objective_on_grid(data[n], ivars[n], template, args, method, true_rvs[n], 1024.)
             best_rvs_hw[n,j] = quadratic_max(rvs, objs)
 
@@ -250,4 +274,4 @@ if __name__ == "__main__":
     plt.xlabel("binary mask half-width")
     plt.ylabel("RV std")
     plt.ylim(0., 500.)
-    plt.savefig("rv_std_hw.png".format(j))
+    plt.savefig(plotprefix+"_rv_std_hw.png".format(j))
